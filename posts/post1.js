2fe8a9880adc01ee6a15b2565d83b040e79cb89c
Moors <136996117+moors-dev@users.noreply.github.com>
2023-06-23 12:48:59 +0200

    Add files via upload

--- posts/post1.js
+++ posts/post1.js
@@ -1,14 +1,14 @@
 const snippets = document.querySelectorAll("pre code");
 
 const code = `
-  // array of random integers
-  let myArr = [];
-  for(let i = 0; i < 10; i++) {
-    // range [-50, 49]
-    myArr.push(Math.floor((Math.random() - 0.5) * 100));
-  }
-  // array of positive integers only
-  let positiveArr = myArr.filter((item) => item > 0);
+// array of random integers
+let myArr = [];
+for(let i = 0; i < 10; i++) {
+  // range [-50, 49]
+  myArr.push(Math.floor((Math.random() - 0.5) * 100));
+}
+// array of positive integers only
+let positiveArr = myArr.filter((item) => item > 0);
 `;
 snippets.forEach(snippet => snippet.textContent = code);
 
